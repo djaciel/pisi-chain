190c06f2f94a81c0b6b5c0638a5ef92069b208f2
djaciel <dj_roses@hotmail.com>
2021-04-30 14:14:59 -0500

    create the miner class

--- src/app/miner.js
+++ src/app/miner.js
@@ -5,4 +5,15 @@ class Miner {
     this.wallet = wallet;
     this.p2pServer = p2pServer;
   }
+
+  mine() {
+    const validTransactions = this.transactionPool.validTransactions();
+    // include a reward for the miner
+    // create a block consisting of the valid transactions
+    // sync the chains in the p2p server
+    // clear the transaction pool
+    // broadcast to every miner to clear their transaction pools
+  }
 }
+
+module.exports = Miner;
